a4e013602ab804d024321dce3b714b8df04d7d33
Mestre Ensinador <jhonatsil@hotmail.com>
2025-06-14 15:03:02 -0400

    Update consultar_saldo.py

--- consultar_saldo.py
+++ consultar_saldo.py
@@ -1,29 +1,9 @@
-import json
-
-def carregar_blockchain():
-    try:
-        with open("blockchain.txt", "r") as f:
-            blockchain = json.load(f)
-        return blockchain
-    except:
-        return []
-
-def calcular_saldo(endereco):
-    blockchain = carregar_blockchain()
+def consultar_saldo(blockchain, endereco):
     saldo = 0
     for bloco in blockchain:
-        transacoes = bloco.get("transacoes", [])
-        for tx in transacoes:
-            if tx["destino"] == endereco:
-                saldo += tx["quantidade"]
-            if tx["origem"] == endereco:
-                saldo -= tx["quantidade"]
-        # Recompensa de mineração (se existir)
-        if bloco.get("minerador") == endereco:
-            saldo += bloco.get("recompensa", 0)
+        for transacao in bloco['transacoes']:
+            if transacao['destino'] == endereco:
+                saldo += transacao['quantidade']
+            if transacao['origem'] == endereco:
+                saldo -= transacao['quantidade']
     return saldo
-
-if __name__ == "__main__":
-    endereco = input("Digite o endereço da carteira para ver saldo: ")
-    saldo = calcular_saldo(endereco)
-    print(f"Saldo da carteira {endereco}: {saldo} $Tib")
